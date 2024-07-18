724c6e721aaa59cc375708f0aba64e655de76b48
Kuma1206 <tadakuma369@gmail.com>
2024-07-18 23:05:06 +0900

    データベースを結合

---
+++ myacount.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang=ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyAcount</title>
+    <link rel="stylesheet" type="text/css" href="css/myacount.css" />
+</head>
+
+<body>
+    <div id="imagebox">
+        <img src="" alt="プロフィール画像" class="profile-image">
+    </div>
+
+    <div>
+        
+    </div>
+
+</body>
+
+</html>
